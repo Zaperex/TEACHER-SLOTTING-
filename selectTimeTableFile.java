26eaedd4f23f8009edc23f1e5088a6ed5a2dda58
mahyartajeri <50030069+mahyartajeri@users.noreply.github.com>
2019-06-12 03:56:12 -0400

    Update selectTimeTableFile.java

--- selectTimeTableFile.java
+++ selectTimeTableFile.java
@@ -1,3 +1,4 @@
+//imports 
 import java.lang.String;
 import java.util.*;
 import java.awt.Desktop;
@@ -18,49 +19,54 @@ import javafx.stage.FileChooser;
 import java.nio.file.*;
 import java.io.*;
 
+//class name
+//seperate class for popup window
 public class selectTimeTableFile {
-  //boolean that determines if button is pressed
-  static boolean init;
-  public static boolean fileSelector() {
-    //creates window
-    Stage window = new Stage();
-    //user has to take care of this window
-    window.initModality(Modality.APPLICATION_MODAL);
-    //sets title
-    window.setTitle("File Selector");
+    //boolean that determines if button is pressed
+    static boolean init;
+    public static  boolean fileSelector() {
+        //creates window
+        Stage window = new Stage();
+        //user has to take care of this window
+        window.initModality(Modality.APPLICATION_MODAL);
+        //sets title
+        window.setTitle("File Selector");
+        
+        //creates VBox layout
+        VBox box = new VBox(20);
+        //sets it to the center
+        box.setAlignment(Pos.CENTER);
+        //button to select file
+        Button initButton = new Button("Select File");
+        //creates label
+        Label warningLabel = new Label("Please select the time table file");
+        
+        //gets label and button and adds it to box
+        box.getChildren().addAll(warningLabel, initButton);
+        
+        //button action
+        initButton.setOnAction(e -> {
+            //sets init as true
+            init = true;
+            //closes window
+            window.close();
+        });
+        
+        //creates Scene with main layout box and dimensions 300 x 100
+        Scene scene = new Scene(box, 300, 100);
+        window.setScene(scene);
+        //waits for user input and displays window
+        window.showAndWait();
+        
+        //returns boolean once button is pressed and file has been selected
+        return init;
+        
+    }
     
-    //creates VBox layout
-    VBox box = new VBox(20);
-    //sets it to the center
-    box.setAlignment(Pos.CENTER);
-    //button to select file
-    Button initButton = new Button("Select File");
-    //creates label
-    Label warningLabel = new Label("Please select the time table file");
     
-    //gets label and button and adds it to box
-    box.getChildren().addAll(warningLabel, initButton);
     
-    //button action
-    initButton.setOnAction(e -> {
-      init = true;
-      window.close();
-    });
     
-    Scene scene = new Scene(box, 300, 100);
-    window.setScene(scene);
-    //waits for user input 
-    window.showAndWait();
     
-    //returns boolean
-    return init;
     
-  }
-  
-  
-  
-  
-  
-  
-  
+    //END
 }
